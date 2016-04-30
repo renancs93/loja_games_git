313599146a43826d5799d43727e77ef2af239582
Renan Silva <renancs93@gmail.com>
2016-04-30 16:57:02 -0300

    criado classes

---
+++ Loja_Games/telaLogin/Classes/Pagamento.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LojaGames.Classes
+{
+    class Pagamento
+    {
+        private int codigo;
+        private string tipo;
+
+        public int Codigo
+        {
+            get
+            {
+                return codigo;
+            }
+
+            set
+            {
+                codigo = value > 0? value : 0;
+            }
+        }
+
+        public string Tipo
+        {
+            get
+            {
+                return tipo;
+            }
+
+            set
+            {
+                tipo = value;
+            }
+        }
+    }
+}

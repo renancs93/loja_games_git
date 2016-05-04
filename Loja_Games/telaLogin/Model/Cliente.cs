f17ce60ae643736b5381ac2c2cf196fc5bdeb98c
Vinicius <vicanalles@hotmail.com>
2016-05-04 15:08:29 -0300

    criação das propriedades da classe cliente.

--- Loja_Games/telaLogin/Model/Cliente.cs
+++ Loja_Games/telaLogin/Model/Cliente.cs
@@ -11,21 +11,46 @@ namespace LojaGames
         private int codigo_cliente;
 
         private string nome;
-        private string cpf; //verificar o tipo da variável
+        private string cpf;
         private string rg;
-        private string data_nascimento; //verificar o tipo da variável
-        private string telefone; //verificar o tipo da variável
+        private DateTime data_nascimento;
+        private string telefone; 
         private string sexo;
         private string status;
         private string email;
-
         private string rua;
         private int numero;
         private string bairro;
         private string estado;
-        private string cep; //verificar o tipo da variável
+        private string cep; 
         private string cidade;
 
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public string RG { get; set; }
+        public DateTime Data { get; set; }
+        public string Telefone { get; set; }
+        public string Sexo { get; set; }
+        public string Status { get; set; }
+        public string Email { get; set; }
+        public string Rua { get; set; }
+        
+        public int Numero
+        {
+            get
+            {
+                return numero;
+            }
 
+            set
+            {
+                numero = value >= 0 ? value : 0;
+            }
+        }
+
+        public string Bairro { get; set; }
+        public string Estado { get; set; }
+        public string CEP { get; set; }
+        public string Cidade { get; set; }
     }
 }
